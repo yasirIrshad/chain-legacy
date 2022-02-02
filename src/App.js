cd159c391626f8f00f2bf26b87403c44e1eb0da5
Mahd Khan <mahdkhan5@gmail.com>
2022-02-02 19:15:20 +0500

    header and footer component add in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import HeaderComponent from './component/HeaderComponent'
+import FooterComponent from './component/FooterComponent'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.jvhgvgvgh
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <HeaderComponent />
+      <FooterComponent />
+    </>
+  )
 }
 
-export default App;
+export default App
