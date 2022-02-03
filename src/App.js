9b6759a949e94d81d17e82fd036ec13c054564f9
Mahd Khan <mahdkhan5@gmail.com>
2022-02-03 20:37:01 +0500

    code merge

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import HeaderComponent from './component/HeaderComponent'
-import FooterComponent from './component/FooterComponent'
 import Banner from './component/BannerComponent';
 import Introduction from './component/introduction';
+import FooterComponent from './component/FooterComponent';
+import AccordionComponent from './component/AccordionComponent';
+import RoadmapComponent from './component/RoadmapComponent';
+import { accordionInfo,roadMap } from './constant/Accordion'
+
 const App = () => {
   return (
     <>
       <HeaderComponent />
       <Banner />
       <Introduction />
-
+      <div className="roadMaoContainer">
+        <RoadmapComponent data={roadMap} />
+      </div>
+      <div className="faq m-0 pb-4 text-white d-flex justify-content-center align-items-end">
+        <h1>F.A.Q</h1>
+      </div>
+      <AccordionComponent data={accordionInfo} />
+      <FooterComponent />
 
     </>
   )
--- src/component/FooterComponent/index.js
+++ src/component/FooterComponent/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import footerImage from '../../assets/image.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDiscord, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import {Image} from 'react-bootstrap';
 
 const FooterComponent = () => {
     return (
-        <div>FooterComponent</div>
+        <div className="footer container-fluid px-5">
+            <div className="px-5">
+                <div className="d-flex align-items-center flex-column mb-3">
+                    <div className='footer-image-container' >
+                        <Image className="img-fluid" src={footerImage} />
+                    </div>
+                    <div className="d-flex justify-content-around iconContainerWidth">
+                        <div className="iconCircle d-flex  align-items-center justify-content-center">
+                            <FontAwesomeIcon icon={faDiscord} />
+                        </div>
+                        <div className="iconCircle d-flex align-items-center justify-content-center">
+                            <FontAwesomeIcon icon={faTwitter} />
+                        </div>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-center footerNavBorder">
+                    <ul className="list-unstyled d-flex justify-content-between flex-sm-row flex-column flex-wrap mt-4 mb-5">
+                        <li className="footerLinks">Introduction</li>
+                        <li className="footerLinks mx-0 mx-sm-5">Roadmap</li>
+                        <li className="footerLinks">FAQ</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 }
 
